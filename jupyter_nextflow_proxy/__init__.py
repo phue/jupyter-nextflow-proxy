02d29b6739e1d467ba1e0ebd593d6520d1ab54a7
Patrick Hüther <huether@imb-mainz.de>
2024-05-29 19:03:45 +0200

    support reading email address from spawner env

--- jupyter_nextflow_proxy/__init__.py
+++ jupyter_nextflow_proxy/__init__.py
@@ -1,12 +1,18 @@
+import os
 from pathlib import Path
 
 def setup_nextflow():
   return {
-    'command': ['nextflow', 'main.nf', '-resume', '--COMMS.enabled=true', '--COMMS.port={port}'],
+    'command': _get_nextflow_cmd,
     'timeout': 120,
-    'environment' : {},
     'launcher_entry': {
         'title': 'Nextflow',
         'icon_path': Path(__file__).parent / 'icons' / 'nextflow.svg'
     }
   }
+
+def _get_nextflow_cmd(port):
+  cmd = ['nextflow', 'main.nf', '-resume', '--COMMS.enabled=true', '--COMMS.port={port}']
+  if 'NXF_MAIL' in os.environ:
+    cmd.append(f'--EMAIL={os.environ["NXF_MAIL"]}')
+  return cmd
