8e7d8ff0ab66e66ba1f496a6d317602ffda490ab
Patrick Hüther <huether@imb-mainz.de>
2024-06-28 14:09:11 +0200

    add support for specifying workflow git rev

--- jupyter_nextflow_proxy/__init__.py
+++ jupyter_nextflow_proxy/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def setup_nextflow():
   return {
-    'command': ['nextflow', 'run', os.environ['NXF_USER_WORKFLOW'], '--PORT={port}', '-resume', '-params-file', os.environ['NXF_USER_PARAMS']],
+    'command': ['nextflow', 'run', os.environ['NXF_USER_WORKFLOW'], '-r', os.environ['NXF_USER_REVISION'], '--PORT={port}', '-resume', '-params-file', os.environ['NXF_USER_PARAMS']],
     'timeout': 120,
     'launcher_entry': {
         'title': 'Nextflow',
